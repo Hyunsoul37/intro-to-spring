c6d1c5639edb46e3077f946b5c15e06e2e91f57c
Hyunsoul37 <soulhyun37@gmail.com>
2023-04-24 16:27:18 +0900

    [ADD] JDBC DB Connection

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,17 +1,28 @@
 package hello.hellospring;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
 import java.util.List;
 import java.util.Optional;
 
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -20,6 +31,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository() ;
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+        return  new JdbcTemplateMemberRepository(dataSource);
     }
 }
